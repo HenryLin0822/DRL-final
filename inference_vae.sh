f9885a8ecf044681b5c92dda5e1fbf78ef9379aa
HenryLin0822 <henrylin.0822@gmail.com>
2025-05-26 15:09:33 +0800

    inference

--- inference_vae.sh
+++ inference_vae.sh
@@ -1,3 +1,4 @@
 #python3 inference_vae.py --checkpoint ./checkpoints/vae/best_model.pt
 #python3 inference_vae.py --checkpoint ./checkpoints/vae/best_model.pt --mode reconstruct --programs "move" "turnLeft"
-python3 inference_vae.py --checkpoint ./checkpoints/vae/best_model.pt --mode reconstruct --programs "WHILE notFacingNorth DO turnLeft END" "move" "REPEAT 3 TIMES move END"
+#python3 inference_vae.py --checkpoint ./checkpoints/vae_fixed/best_model.pt --mode reconstruct --programs "WHILE notFacingNorth DO turnLeft END" "move" "REPEAT 3 TIMES move END"
+python3 inference_vae.py --checkpoint ./checkpoints/vae_fixed/best_model.pt --mode reconstruct --programs "DEF run m( move m)" "DEF run m( turnLeft turnRight m)"
